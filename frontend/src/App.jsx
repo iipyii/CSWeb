056eba4e4c6ed01cd55b8b2df5d4e6c447fa7f43
iipyii <piyananice@gmail.com>
2026-01-14 23:21:07 +0700

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        TAILWIND WORKS
+      </h1>
+    </div>
+  );
+}
